2c80e218e44c5562b4cc54e7bf182cff424b6e9b
melisha-git <egorka_savelev_2001@rambler.ru>
2021-07-13 14:42:09 +0300

    testMap 13.07.21

--- containers/map/test_map.cpp
+++ containers/map/test_map.cpp
@@ -3,11 +3,14 @@
 #include <map>
 
 void testMap() {
+	/*
+	 * DEFAULT VECTOR TEST
+	 */
 	std::map<int, int> v1;
-    v1[1] = 8;
-    v1[2] = 2;
-    v1[3] = 3;
-    v1[10] = 10;
+    v1.insert(std::pair<int, int>(8, 8));
+    v1.insert(std::pair<int, int>(2, 2));
+    v1.insert(std::pair<int, int>(3, 3));
+    v1.insert(std::pair<int, int>(10, 10));
 	std::cout << "\x1b[31mDEFAULT VECTOR TEST\n";
 	std::cout << v1.size() << std::endl;
     std::cout << std::endl;
@@ -18,15 +21,15 @@ void testMap() {
 	std::cout << std::endl;
 
 	ft::map<int, int> g1;
-    g1[1] = 8;
-    g1[2] = 2;
-    g1[3] = 3;
-    g1[10] = 10;
+    g1.insert(ft::pair<int, int>(8, 8));
+    g1.insert(ft::pair<int, int>(2, 2));
+    g1.insert(ft::pair<int, int>(3, 3));
+    g1.insert(ft::pair<int, int>(10, 10));
 	std::cout << g1.size() << std::endl;
 	std::cout << std::endl;
 	/*
-	 * CLEAR
-	 */
+	* CLEAR
+	*/
 	std::cout << "\x1b[34mCLEAR TEST";
 	std::cout << std::endl;
 	v1.clear();
@@ -39,4 +42,33 @@ void testMap() {
 		std::cout << "\x1b[32mMy vector is clear\n";
 	}
 	std::cout << g1.size() << std::endl;
+}
+
+void testMapFindErase() {
+	/*
+	 * DEFAULT VECTOR TEST
+	*/
+	std::map<int, int> v1;
+    v1.insert(std::pair<int, int>(8, 8));
+    v1.insert(std::pair<int, int>(2, 2));
+    v1.insert(std::pair<int, int>(3, 3));
+    v1.insert(std::pair<int, int>(10, 10));
+	std::cout << "\x1b[31mDEFAULT VECTOR TEST\n";
+	std::cout << v1.size() << std::endl;
+	/*
+	 * MY VECTOR TEST
+	 */
+	std::cout << "\x1b[32mMY VECTOR TEST";
+	std::cout << std::endl;
+
+	ft::map<int, int> g1;
+    g1.insert(ft::pair<int, int>(8, 8));
+    g1.insert(ft::pair<int, int>(2, 2));
+    g1.insert(ft::pair<int, int>(3, 3));
+    g1.insert(ft::pair<int, int>(10, 10));
+	std::cout << g1.size() << std::endl;
+	std::cout << std::endl;
+	/*
+	* FIND
+	*/
 }
